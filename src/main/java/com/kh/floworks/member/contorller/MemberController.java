6b04076c24b6fdbb18b2fdaab8ac81480a53bb63
kyj <fioonmp1001@gmail.com>
2021-05-10 00:26:54 +0900

    security 설정 추가 / 로그인 페이지 추가

---
+++ src/main/java/com/kh/floworks/member/contorller/MemberController.java
@@ -0,0 +1,13 @@
+package com.kh.floworks.member.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@GetMapping("/login")
+	public void login() {}
+}
