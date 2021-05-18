cc9eb45299f0b1969c3898ca78409fbff3306907
sayoungri <bori201905@gmail.com>
2021-05-18 09:21:25 +0900

    fullcalendar jsp, calendar 컨트롤러와 vo,Service,dao 추가했습니다. 아직 db작업전. 리소스에 풀캘린더 폴더 추가하였음

---
+++ src/main/java/com/kh/floworks/calendar/model/vo/Calendar.java
@@ -0,0 +1,19 @@
+package com.kh.floworks.calendar.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Calendar {
+	
+	private int calendarNo;
+	private String id;
+	private String subject;
+	private String startDate;
+	private String endDate;
+	private String memo; //메모안에 장소도 적을 수 있음
+	
+}
