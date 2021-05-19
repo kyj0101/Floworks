b0449c76c1e05a406d947158094035efee005c09
Yujeong Kang <63092179+kyj0101@users.noreply.github.com>
2021-05-19 18:54:52 +0900

    Revert "이메일 외부 발송 기능 추가  (#41)"
    This reverts commit dfc8497fa97e48fb4471e839c0d106ed34d17a8e.

--- src/main/java/com/kh/floworks/email/model/service/EmailService.java
+++ src/main/java/com/kh/floworks/email/model/service/EmailService.java
@@ -1,7 +1,12 @@
 package com.kh.floworks.email.model.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.kh.floworks.email.model.vo.Email;
 
@@ -9,10 +14,6 @@ import com.kh.floworks.email.model.vo.Email;
 public interface EmailService {
 	
 	int insertEmail(Email email);
-
-	int insertFile(Map<String, String> fileMap);
-
-	int insertDraftEmail(Email email);
 
 	List<Email> selectSentList(String id);
 
@@ -22,19 +23,23 @@ public interface EmailService {
 	
 	Map<String, String> selectFile(int fileNo);
 
+	int insertFile(Map<String, String> fileMap);
+
 	List<String> selectRecipientList(Map<String, String> param);
+
+	int insertDraftEmail(Email email);
 
 	List<Email> selectDraftList(String id);
 
 	Email selectOneDraftEmail(int emailNo);
 
-	List<Map<String, String>> selectFileList();
-
 	int updateDraftFile(Map<String, Object> param);
+
+	int deleteFile(int fileNo);
 
 	int updateDraft(Email email);
 
-	int deleteFile(int fileNo);
+	List<Map<String, String>> selectFileList();
 
 	int deleteDraft(int emailNo);
 
