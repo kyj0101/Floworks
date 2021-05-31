6eddf6296a87b17fce9c97fed88a06aa5a4be2a0
jjh9251 <49854588+jjh9251@users.noreply.github.com>
2021-05-31 09:43:41 +0900

    Jjh (#82)
    * AttendController / AttendService / AttendDao 클래스 생성
    
    * footer 근태 경로 조정 / controller, service, dao 수정
    
    * controller, jsp 경로 수정
    
    * 연차신청 버튼 링크 추가
    
    * admin : controller , service , dao 생성 및 attendList.jsp 생성 / footer : 관리자
    설정 링크 수정
    
    * 근태 관리 페이지 jsp 수정 / Calendar 추가 / 현재시간 추가 / css 수정
    
    Co-authored-by: Jinhong <Jinhong@DESKTOP-UNV076I>

--- src/main/java/com/kh/floworks/attendance/controller/AttendanceController.java
+++ src/main/java/com/kh/floworks/attendance/controller/AttendanceController.java
@@ -9,10 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import com.kh.floworks.attendance.model.service.AttendService;
-import com.kh.floworks.authentication.email.controller.EmailAuthenticationController;
 import com.kh.floworks.member.model.vo.Member;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,16 +25,11 @@ public class AttendanceController {
 	private AttendService attendService;
 	
 	@GetMapping("/attendanceView.do")
-	public void attendanceView(Authentication authentication, @AuthenticationPrincipal Member member, Model model) {
-		
-		
-		
-		//1. 사용자입력값
-		
-		//2. 업무로직
-		
-		//3. jsp처리 위임
+	public void attendanceView(Authentication authentication,
+							   @AuthenticationPrincipal Member member,
+							   Model model) {
 		
 		model.addAttribute("loginMember", authentication.getPrincipal());
 	}
+	
 }
