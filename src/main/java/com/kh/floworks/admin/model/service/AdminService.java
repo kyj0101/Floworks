298dcf1cb06a60da40200e1aa007255affd254b6
Yujeong Kang <63092179+kyj0101@users.noreply.github.com>
2021-06-01 09:59:20 +0900

    Revert "Jjh (#92)" (#95)
    This reverts commit 7c80e03898cbeb789e62d67256ad6dae397c6614.

--- src/main/java/com/kh/floworks/admin/model/service/AdminService.java
+++ src/main/java/com/kh/floworks/admin/model/service/AdminService.java
@@ -1,15 +1,5 @@
 package com.kh.floworks.admin.model.service;
 
-import java.util.List;
-
-import com.kh.floworks.admin.model.vo.UserDetail;
-import com.kh.floworks.admin.model.vo.UserList;
-
 public interface AdminService {
-
-	List<UserList> selectUserList();
-
-	List<UserDetail> selectOneDetail();
-
 
 }
