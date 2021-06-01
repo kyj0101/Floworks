7c80e03898cbeb789e62d67256ad6dae397c6614
jjh9251 <49854588+jjh9251@users.noreply.github.com>
2021-06-01 09:44:56 +0900

    Jjh (#92)
    * AttendController / AttendService / AttendDao 클래스 생성
    
    * footer 근태 경로 조정 / controller, service, dao 수정
    
    * controller, jsp 경로 수정
    
    * 연차신청 버튼 링크 추가
    
    * admin : controller , service , dao 생성 및 attendList.jsp 생성 / footer : 관리자
    설정 링크 수정
    
    * 근태 관리 페이지 jsp 수정 / Calendar 추가 / 현재시간 추가 / css 수정
    
    * 추가 : memberList.jsp , attendList.jsp ,  Users(admin-vo) / 수정 : footer ,
    adminController
    
    * admin - vo 수정
    
    * 회원 전체 목록 조회페이지 수정
    컬럼 수정
    
    * admin controller, service, serviceImpl, footer 맵핑 수정
    
    Co-authored-by: Jinhong <Jinhong@DESKTOP-UNV076I>

--- src/main/java/com/kh/floworks/admin/model/service/AdminService.java
+++ src/main/java/com/kh/floworks/admin/model/service/AdminService.java
@@ -1,5 +1,15 @@
 package com.kh.floworks.admin.model.service;
 
+import java.util.List;
+
+import com.kh.floworks.admin.model.vo.UserDetail;
+import com.kh.floworks.admin.model.vo.UserList;
+
 public interface AdminService {
+
+	List<UserList> selectUserList();
+
+	List<UserDetail> selectOneDetail();
+
 
 }
