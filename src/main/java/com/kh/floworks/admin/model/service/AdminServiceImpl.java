b771130929a722c96fe2b1cb39eaa0483f9a0e77
Jinhong <Jinhong@DESKTOP-UNV076I>
2021-06-01 09:11:54 +0900

    admin controller, service, serviceImpl, footer 맵핑 수정

--- src/main/java/com/kh/floworks/admin/model/service/AdminServiceImpl.java
+++ src/main/java/com/kh/floworks/admin/model/service/AdminServiceImpl.java
@@ -1,9 +1,13 @@
 package com.kh.floworks.admin.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.floworks.admin.model.dao.AdminDao;
+import com.kh.floworks.admin.model.vo.UserDetail;
+import com.kh.floworks.admin.model.vo.UserList;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,5 +16,20 @@ import lombok.extern.slf4j.Slf4j;
 public class AdminServiceImpl implements AdminService {
 
 	@Autowired
-	AdminDao adminDao;
+	private AdminDao adminDao;
+
+	
+	@Override
+	public List<UserList> selectUserList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public List<UserDetail> selectOneDetail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
--- src/main/java/com/kh/floworks/admin/model/vo/UserDetail.java
+++ src/main/java/com/kh/floworks/admin/model/vo/UserDetail.java
@@ -7,14 +7,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Users {
-
+public class UserDetail {
 
 	private String workspaceId;
 	private String id;
 	private String name;
 	private String department;
 	private String position;
+	private String phone;
+	private String address;
+	private String email;
 	private String hireDate;
 	private String resignDate;
 	private char resignYN;
