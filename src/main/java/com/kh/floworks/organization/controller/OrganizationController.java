958ad374a3b8ab2d471031d9e9f3167e4c21d8e5
Jinhong <Jinhong@DESKTOP-UNV076I>
2021-06-05 01:13:30 +0900

    컨트롤러 충돌 제거

--- src/main/java/com/kh/floworks/organization/controller/OrganizationController.java
+++ src/main/java/com/kh/floworks/organization/controller/OrganizationController.java
@@ -8,11 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-import org.springframework.ui.Model;
-=======
 
->>>>>>> branch 'master' of https://github.com/kyj0101/Floworks.git
+import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
